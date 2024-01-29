d2f34b9cbd22410e028c73075d76d47437d45e00
KhantYadanarMoe <108444110+KhantYadanarMoe@users.noreply.github.com>
2024-01-29 18:57:07 +0630

    Update HTTP.php

--- _classes/Helpers/HTTP.php
+++ _classes/Helpers/HTTP.php
@@ -3,7 +3,7 @@
 namespace Helpers;
 
 class HTTP{
-    static $base = "http://localhost/simple-expenses/project";
+    static $base = "http://localhost/simple-expenses";
 
     static function redirect($path, $q = ""){
         $url = static::$base . $path; //static::$base = "https://localhost/fairway/project", $path = user_input page
